3199c4071add7ba8a734c9d11e8245f4ab02a5c3
John Wallace <John.Wallace@noaa.gov>
2016-09-16 15:31:59 -0700

    Create Git.World.f.HiRez.R

---
+++ R/Git.World.f.HiRez.R
@@ -0,0 +1,23 @@
+Git.World.f.HiRez <- function() {
+
+   usr.warn <- options(warn = -1)
+   on.exit(options(usr.warn))   
+     
+   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.land.A.RData", "world.f.land.A.RData")
+   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.land.B.RData", "world.f.land.B.RData")
+   download.file("https://cdn.rawgit.com/John-R-Wallace/GSHHG_High_Rez_for_R/master/world.f.borders.lakes.rivers.RData", "world.f.borders.lakes.rivers.RData")
+
+   load("world.f.land.A.RData")
+   load("world.f.land.B.RData")
+   load("world.f.borders.lakes.rivers.Rdata")
+
+  
+   assign("world.f.land", rbind(world.f.land.A, world.f.land.B), pos = 1)
+   remove(world.f.land.A, world.f.land.B, pos = 1)
+   
+   file.remove("world.f.land.A.RData", "world.f.land.B.RData", "world.f.borders.lakes.rivers.Rdata")
+
+   base::ls(pattern = 'world', pos = 1)
+}
+
+
