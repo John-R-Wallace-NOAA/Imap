fd8e1ab96de4b22bc180d3e90cdb5250766a934a
John Wallace <John.Wallace@noaa.gov>
2016-09-16 15:35:31 -0700

    Update col.alpha.R

--- R/col.alpha.R
+++ R/col.alpha.R
@@ -1,6 +1,10 @@
-col.alpha <- function (col, alpha=0.5) 
+function (col, alpha = 0.5) 
 {
+    FUNC <- function(col, alpha = alpha) {
         COL <- col2rgb(col)/255
-        rgb(red=COL[1], green=COL[2], blue=COL[3], alpha=alpha)
+        rgb(red = COL[1], green = COL[2], blue = COL[3], alpha = alpha)
+    }
+    for (i in 1:length(col)) col[i] <- FUNC(col[i], alpha = alpha[i %r1% 
+        length(alpha)])
+    col
 }
-
