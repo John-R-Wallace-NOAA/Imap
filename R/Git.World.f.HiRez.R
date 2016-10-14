f1ebdcb246cc9fa3cb866400388af6b55216e6c0
John Wallace <John.Wallace@noaa.gov>
2016-10-14 00:28:42 -0700

    Update Git.World.f.HiRez.R

--- R/Git.World.f.HiRez.R
+++ R/Git.World.f.HiRez.R
@@ -9,7 +9,7 @@ Git.World.f.HiRez <- function() {
 
    base::load("world.f.land.A.RData")
    base::load("world.f.land.B.RData")
-   base::load("world.f.borders.lakes.rivers.RData")
+   base::load("world.f.borders.lakes.rivers.RData", 1)
 
   
    assign("world.f.land", rbind(world.f.land.A, world.f.land.B), pos = 1)
