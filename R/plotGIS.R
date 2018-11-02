7e9f4be73de81c0030c0d4d06a2c48daec922d3a
John Wallace <John.Wallace@noaa.gov>
2018-11-02 14:23:06 -0700

    moved verbose arg

--- R/plotGIS.R
+++ R/plotGIS.R
@@ -1,5 +1,5 @@
 plotGIS <- function (LongLat = NULL, polygons = NULL, longrange = c(-126, -124), latrange = c(41.5, 43.5), 
-    layer = c('etopo1', 'etopo1_bedrock', 'crm', 'SoCal_1as')[1], autoLayer = TRUE, quiet = TRUE, landOverlay = TRUE, verbose = FALSE, col.imap = "grey40", alphaRaster = 1, col.pts = "red", pch.pts = 16, 
+    layer = c('etopo1', 'etopo1_bedrock', 'crm', 'SoCal_1as')[1], autoLayer = TRUE, verbose = FALSE, quiet = TRUE, landOverlay = TRUE, col.imap = "grey40", alphaRaster = 1, col.pts = "red", pch.pts = 16, 
     cex.pts = 0.25, col.poly = col.alpha((grDevices::colorRampPalette(colors = c("darkblue", "blue", "lightblue",
     "lightgreen", "yellow", "orange", "red")))(length(polygons)), alpha), alpha = 0.75, border.poly = NULL, 
     lwd.poly = 1.5, Fname = NULL, levels.contour = if(landOverlay) seq(-100, -2000, by = -100) else seq(-11000, 9000, by = 500),
