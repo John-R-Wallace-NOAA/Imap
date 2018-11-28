c5a373fc214377150151bbed133b04969062e176
John Wallace <John.Wallace@noaa.gov>
2018-11-28 15:47:57 -0800

    yOnly => latOnly

--- R/imap.ll.R
+++ R/imap.ll.R
@@ -70,7 +70,7 @@ imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic 
             axis(2, at = yticks, labels = lat.labels, srt = 90)
             axis(4, at = yticks, labels = lat.labels, srt = 90)
         }
-        if (axes == "yOnly") {
+        if (axes == "latOnly") {
             do.call(plot, c(list(x = longrange, y = latrange, xlab = "", ylab = "Latitude", type = "n", xaxt = "n", yaxt = "n"), plot.dots))
             lat.labels <- ifelse(yticks == 0, "0", ifelse(yticks > 0, paste(yticks, ifelse(oz, "S", "N"), sep = ""), paste(-yticks, ifelse(oz, "N", "S"), sep = "")))
             axis(2, at = yticks, labels = lat.labels, srt = 90)
