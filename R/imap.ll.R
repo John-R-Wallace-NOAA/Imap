2945e6ad9b445ad43d6bae549ca911596e1197b3
John Wallace <John.Wallace@noaa.gov>
2020-10-19 19:16:54 -0700

    Changed with rgit

--- R/imap.ll.R
+++ R/imap.ll.R
@@ -1,5 +1,6 @@
-imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic = FALSE, arctic = FALSE, oz = FALSE, axes = "map", grid = FALSE, grid.col = col.alpha('grey34'),
-    aspect = 1.5, add = FALSE, zoom = TRUE, lines.out.of.bounds = TRUE, tol = 0.05, cex.xlab = 1, cex.ylab = 1, cex.axis = 1, lwd.ticks = 1, last = TRUE, ...) 
+imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic = FALSE, arctic = FALSE, oz = FALSE, axes = "map", grid = FALSE, grid.col = col.alpha("grey34"),
+    aspect = 1.5, add = FALSE, zoom = TRUE, lines.out.of.bounds = TRUE, tol = 0.05, xlab = list("Longitude", cex = cex.xlab), ylab = list("Latitude", cex = cex.ylab), 
+    cex.xlab = 1, cex.ylab = 1, cex.axis = 1, lwd.ticks = 1, last = TRUE, pin = NULL, ...) 
 {
     all.dots <- list(...)
     plot.dots <- all.dots[grepl("plt.", names(all.dots))]
@@ -50,38 +51,66 @@ imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic 
         if (aspect <= 0) 
             stop("Aspect ratio must be greater than zero.")
         par(new = TRUE, pty = "m")
-        usr <- par()$usr
-        pin <- c(0.76, 0.76) * par()$din
-        ud <- c(usr[2] - usr[1], usr[4] - usr[3])
-        x <- ((1/aspect) * ud[1] * pin[2])/ud[2]
-        if (x <= pin[1]) 
-            par(pin = c(x, pin[2]))
-        else par(pin = c(pin[1], (aspect * ud[2] * pin[1])/ud[1]))
+        if(is.null(pin)) {
+            usr <- par()$usr
+            pin <- c(0.76, 0.76) * par()$din
+            ud <- c(usr[2] - usr[1], usr[4] - usr[3])
+            x <- ((1/aspect) * ud[1] * pin[2])/ud[2]
+            if (x <= pin[1]) 
+                par(pin = c(x, pin[2]))
+            else par(pin = c(pin[1], (aspect * ud[2] * pin[1])/ud[1]))
+        } else
+           par(pin = pin)
+        
         xaxp <- par()$xaxp
         xticks <- round(seq(xaxp[1], xaxp[2], len = xaxp[3] + 1), 5)
         yaxp <- par()$yaxp
         yticks <- round(seq(yaxp[1], yaxp[2], len = yaxp[3] + 1), 5)
-        if (axes == "map") {
-            do.call(plot, c(list(x = longrange, y = latrange, xlab = list("Longitude", cex = cex.xlab), ylab = list("Latitude", cex = cex.ylab), type = "n", xaxt = "n", yaxt = "n"), plot.dots))
+        
+        if(length(axes) == 1 && axes == "map") axes <- 1:4
+        if(length(axes) == 1 && axes == "latOnly") axes <- c(2, 4)
+        if(length(axes) == 1 && axes == "std") axes <- 0
+        
+        if(any(axes %in% 1:4)) {
+            do.call(plot, c(list(x = longrange, y = latrange, xlab = xlab, ylab = ylab, type = "n", xaxt = "n", yaxt = "n"), plot.dots))
             long.labels <- ifelse(abs(xticks) == 180, "180", ifelse(xticks == 0, "0", ifelse(xticks > 0, paste(xticks, "E", sep = ""), 
                 ifelse(xticks < -180, paste(xticks + 360, "E", sep = ""), paste(-xticks, "W", sep = "")))))
             lat.labels <- ifelse(yticks == 0, "0", ifelse(yticks > 0, paste(yticks, ifelse(oz, "S", "N"), sep = ""), paste(-yticks, ifelse(oz, "N", "S"), sep = "")))
-            axis(1, at = xticks, labels = long.labels, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
-            axis(3, at = xticks, labels = long.labels, mgp = c(3, 0.5, 0), cex.axis = cex.axis, lwd.ticks = lwd.ticks)
-            axis(2, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
-            axis(4, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
+            
+            if(any(axes %in% 1))
+               axis(1, at = xticks, labels = long.labels, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
+               
+            if(any(axes %in% 3))
+               axis(3, at = xticks, labels = long.labels, mgp = c(3, 0.5, 0), cex.axis = cex.axis, lwd.ticks = lwd.ticks)   
+                      
+            if(any(axes %in% 2))
+              axis(2, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
+              
+            if(any(axes %in% 4))  
+              axis(4, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
         }
-        if (axes == "latOnly") {
-            do.call(plot, c(list(x = longrange, y = latrange, xlab = "", ylab = list("Latitude", cex = cex.ylab), type = "n", xaxt = "n", yaxt = "n"), plot.dots))
-            lat.labels <- ifelse(yticks == 0, "0", ifelse(yticks > 0, paste(yticks, ifelse(oz, "S", "N"), sep = ""), paste(-yticks, ifelse(oz, "N", "S"), sep = "")))
-            # print(yticks); print(lat.labels)
-            axis(2, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
-            axis(4, at = yticks, labels = lat.labels, srt = 90, cex.axis = cex.axis, lwd.ticks = lwd.ticks)
+        
+        if(length(axes) == 1 && axes == 0)
+                do.call(plot, c(list(x = longrange, y = latrange, xlab = list(ifelse(is.null(dimnames(area)[[2]]), "", dimnames(area)[[2]][1]), cex = cex.xlab), 
+                   ylab = list(ifelse(is.null(dimnames(area)[[2]]), "", dimnames(area)[[2]][2]), cex = cex.ylab)), type = "n", plot.dots))
+                   
+        if (grid & last) {
+            xaxp <- par()$xaxp
+            xticks <- round(seq(xaxp[1], xaxp[2], len = xaxp[3] + 1), 5)
+            yaxp <- par()$yaxp
+            yticks <- round(seq(yaxp[1], yaxp[2], len = yaxp[3] + 1), 5)
+            
+            if(any(axes %in% c(2, 4)) & !any(axes %in% c(1, 3)))
+               abline(h = yticks, lty = 1, lwd = 0, col = grid.col) 
+            else  {
+               if(!any(axes %in% c(2, 4)) & any(axes %in% c(1, 3)))
+                  abline(v = xticks, lty = 1, lwd = 0, col = grid.col) 
+               else 
+                  abline(v = xticks, h = yticks, lty = 1, lwd = 0, col = grid.col)   
+            }      
         }
-        if (axes == "std") 
-            do.call(plot, c(list(x = longrange, y = latrange, xlab = list(ifelse(is.null(dimnames(area)[[2]]), "", dimnames(area)[[2]][1]), cex = cex.xlab), 
-               ylab = list(ifelse(is.null(dimnames(area)[[2]]), "", dimnames(area)[[2]][2]), cex = cex.ylab)), type = "n", plot.dots))
     }
+    
     lat[is.na(lat)] <- latrange[1] + .Machine$double.eps
     long[is.na(long)] <- longrange[1] + .Machine$double.eps
     tf <- (long >= longrange[1] & long <= longrange[2]) & (lat >= latrange[1] & lat <= latrange[2])
@@ -94,6 +123,7 @@ imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic 
     else tf.na <- NA
     "print(c(length(tf), sum(tf)))"
     "print(sum(tf)/length(tf))"
+    
     if(lines.out.of.bounds) {
         if(!is.na(poly))
              polygon(x = long, y = lat, col = poly, border = NA)
@@ -105,20 +135,8 @@ imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic 
       cat("\n\nNew Edits\n\n")        
     } 
     
-    if (grid & last) {
-        xaxp <- par()$xaxp
-        xticks <- round(seq(xaxp[1], xaxp[2], len = xaxp[3] + 1), 5)
-        yaxp <- par()$yaxp
-        yticks <- round(seq(yaxp[1], yaxp[2], len = yaxp[3] + 1), 5)
-        
-        if(axes == 'latOnly') 
-           abline(h = yticks, lty = 1, lwd = 0, col = grid.col) 
-        else    
-           abline(v = xticks, h = yticks, lty = 1, lwd = 0, col = grid.col)   
-    }     
-  
     z.area <- area[tf, drop = TRUE][tf.na, drop = FALSE]
-
+    
     if (zoom) {
         if (is.list(c1 <- locator(1))) {
             points(c1, pch = 3, cex = 3, col = 2)
@@ -138,6 +156,7 @@ imap.ll <- function (area = npacific, longrange, latrange, poly = NA, antarctic 
     }
     invisible(matrix(z.area, ncol = 2))
 }
+
 
 
 
