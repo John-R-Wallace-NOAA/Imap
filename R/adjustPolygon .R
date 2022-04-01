48b72baea29f390d3d80dd1dd24c9a591dda58e9
John Wallace <John.Wallace@noaa.gov>
2022-03-31 18:21:21 -0700

    Now can handle data frames

--- R/adjustPolygon .R
+++ R/adjustPolygon .R
@@ -1,19 +1,29 @@
-
- adjustPolygon <- function (xy, colLine = "red", colPoly = "purple", alpha = 0.25, closePoly = TRUE, lty = 1, colBg = par()$bg, ...)  {
- 
-    lines(xy$x, xy$y, col = colLine, lty = lty, type = "o", ...)
-    while (length(PtRow <- identify(xy, labels = "", n = 1)) == 1) {
+adjustPolygon <- function (xy, colLine = "red", colPoly = "purple", alpha = 0.25, closePoly = TRUE, lty = 1, colBg = par()$bg, ...)  {
+    
+    X <- xy[,1]
+    Y <- xy[,2]
+    
+    lines(X, Y, col = colLine, lty = lty, type = "o", ...)
+    
+    while (length(PtRow <- identify(X, Y, labels = "", n = 1)) == 1) {
         newPt <- locator(1)
-        lines(xy$x, xy$y, lty = lty, col = ifelse(colBg == "transparent", 
-            "white", colBg), type = "o", ...)
-        xy[PtRow, ] <- newPt
-        lines(xy$x, xy$y, lty = lty, col = colLine, type = "o", ...)
+        lines(X, Y, lty = lty, col = ifelse(colBg == "transparent", "white", colBg), type = "o", ...)
+        X[PtRow] <- newPt$x
+        Y[PtRow] <- newPt$y
+        lines(X, Y, lty = lty, col = colLine, type = "o", ...)
     }
-    lines(xy$x, xy$y, lty = lty, col = ifelse(colBg == "transparent", "white", colBg), type = "o", ...)
+    
+    lines(X, Y, lty = lty, col = ifelse(colBg == "transparent", "white", colBg), type = "o", ...)
     if (closePoly) {
-        xy$x[length(xy$x)] <- xy$x[1]
-        xy$y[length(xy$y)] <- xy$y[1]
+        X[length(X)] <- X[1]
+        Y[length(Y)] <- Y[1]
     }
-    polygon(xy$x, xy$y, col = col.alpha(colPoly, alpha), lty = lty, ...)
-    invisible(xy)
+    
+    polygon(X, Y, col = col.alpha(colPoly, alpha), lty = lty, ...)
+    
+    out <- cbind(X, Y)
+    colnames(out) <- colnames(xy)
+    if(is.data.frame(xy))
+         out <- data.frame(out)
+    invisible(out)
 }
